dfc8951ce84208269bac275e3523c150c54e9875
Joel Verhagen <jver@microsoft.com>
2023-12-02 14:16:13 -0500

    Try

--- TestCredentialProvider/Program.cs
+++ TestCredentialProvider/Program.cs
@@ -218,7 +218,7 @@ class SetLogLevelRequestHandler : RequestHandlerBase<SetLogLevelRequest, SetLogL
     public override Task<SetLogLevelResponse> HandleRequestAsync(SetLogLevelRequest request, CancellationToken cancellationToken)
     {
         _logger.LogLevel = request.LogLevel;
-        _logger.Log(LogLevel.Verbose, $"Setting log level to {request.LogLevel}.");
+        // _logger.Log(LogLevel.Verbose, $"Setting log level to {request.LogLevel}.");
         return Task.FromResult(new SetLogLevelResponse(MessageResponseCode.Success));
     }
 }
